75013e124358b6f355df9e4b0d49dfae31ab702d
jk1985yc <33709163+jk1985yc@users.noreply.github.com>
2021-09-30 13:18:51 +0800

    Update kibana.sh

--- kibana.sh
+++ kibana.sh
@@ -3,6 +3,7 @@
 IP=127.0.0.1
 PORT=5601
 ID=My-Patterns-*
+#ID=My-Patterns-`(date +%Y.%m)`-* #Using Crontab Auto Generate Date. ex: My-Patterns-2021.10-*
 
 curl -X POST "${IP}:${PORT}/api/saved_objects/index-pattern/${ID}" -H 'kbn-xsrf: true' -H 'Content-Type: application/json' -d'
 {
