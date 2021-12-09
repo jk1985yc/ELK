149edcf0704f49602be95b5a110ce08cf37610b0
jk1985yc <33709163+jk1985yc@users.noreply.github.com>
2021-12-09 12:49:34 +0800

    Update CreateIndexPatterns.sh

--- CreateIndexPatterns.sh
+++ CreateIndexPatterns.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+TYPE_NAME=${name for logstash out put}
 IP=127.0.0.1
 PORT=5601
-ID=dev-`(date +%Y.%m)`.*
+ID=${TYPE_NAME}-`(date +%Y.%m)`.*
 
 curl -X POST "${IP}:${PORT}/api/saved_objects/index-pattern/${ID}" -H 'kbn-xsrf: true' -H 'Content-Type: application/json' -d'
 {
