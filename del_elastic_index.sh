b7b47193c629498668cc793682113adfc253cdbd
jk1985yc <33709163+jk1985yc@users.noreply.github.com>
2021-12-10 09:17:28 +0800

    Update del_elastic_index.sh

--- del_elastic_index.sh
+++ del_elastic_index.sh
@@ -3,9 +3,9 @@
 IP=127.0.0.1
 PORT=9200
 
-#clear 15 days ago data
-LAST_DATA=`date -d "-15 days" "+%Y.%m.%d"`
+#clear num days ago index
+LAST_DATA=`date -d "-$1 days" "+%Y.%m.%d"`
 
 
 curl -X DELETE "${IP}:${PORT}/*-${LAST_DATA}"
-curl -X GET "${IP}:${PORT}/_cat/indices/?v"
+#curl -X GET "${IP}:${PORT}/_cat/indices/?v"
