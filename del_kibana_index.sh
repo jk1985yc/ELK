a042681cd6e35aba79ef43150cc98b1c24545793
jk1985yc <33709163+jk1985yc@users.noreply.github.com>
2021-12-10 09:25:32 +0800

    Update del_kibana_index.sh

--- del_kibana_index.sh
+++ del_kibana_index.sh
@@ -2,7 +2,7 @@ IP=127.0.0.1
 TYPE_NAME=$index_name #need exact match
 PORT=5601
 
-#clear $num months ago indexi, if need del before 1, please change the $var number.
+#clear $num months ago indexi
 LAST_DATA=`date -d "-$1 months" "+%Y.%m".*`
 
 
