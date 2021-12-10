d821cdc34393cd68ec4cd04936067bc1c4adf00b
jk1985yc <33709163+jk1985yc@users.noreply.github.com>
2021-12-10 08:55:26 +0800

    Create del_kibana_index.sh

---
+++ del_kibana_index.sh
@@ -0,0 +1,9 @@
+IP=127.0.0.1
+TYPE_NAME=$index_name #need exact match
+PORT=5601
+
+#clear 1 months ago indexi, if need del before 1, please change the $var number.
+LAST_DATA=`date -d "-1 months" "+%Y.%m".*`
+
+
+curl -X DELETE "${IP}:${PORT}/api/saved_objects/index-pattern/${TYPE_NAME}-${LAST_DATA}" -H 'kbn-xsrf: true'
